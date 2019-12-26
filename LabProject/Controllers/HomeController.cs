df10898f5c69a3e6ba7324dd2d8b9215d7f08174
Rasakaz <nivazenkot@gmail.com>
2019-12-26 14:51:42 +0200

    first upload

---
+++ LabProject/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabProject.Models;
+
+namespace LabProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag["userName"] = user.userName;
+                ViewBag["password"] = user.password;
+            }
+            return View();
+        }
+
+    }
+}
---
+++ LabProject/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabProject.Models
+{
+    public class User
+    {
+
+        [Required]
+        public string userName { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
